91e8dc0eb0d48fb0b280f11b5cd244aa8fe749c7
Maia Federica_G <maiafedericag@gmail.com>
2023-11-01 11:54:49 -0300

    Ejercicio 9 realizado y verificado

--- js/Ejercicios/09-ejercicio.js
+++ js/Ejercicios/09-ejercicio.js
@@ -2,6 +2,20 @@
 //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 
 
-const frase = prompt ('Ingrese una frase');
+const frase = prompt ('Ingrese una frase:');
 
 console.log (frase.length);
+console.log (frase.charAt(0));
+
+let vocales = "";
+
+for (let i = 0; i < frase.length; i++) {
+  const caracter = frase.charAt(i);
+
+  if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+
+    vocales += caracter;
+  }
+}
+document.write("Las vocales en la frase son: " + vocales);
+
