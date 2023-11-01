7c2201373d4b1dd206ae4b506d912616c6f99ac7
Maia Federica_G <maiafedericag@gmail.com>
2023-11-01 12:00:42 -0300

    Comence a realizar el ejercicio

---
+++ js/Ejercicios/10-ejercicio.js
@@ -0,0 +1,5 @@
+//10.- Escribe un programa que pida un número y nos diga si es divisible 
+//por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+
+const numeroPrueba = parseInt(prompt('Ingrese un numero:'));
+
