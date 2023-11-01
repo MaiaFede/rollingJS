e3de7c33c31349e8891786f6d516fb70f1d4c348
Maia Federica_G <maiafedericag@gmail.com>
2023-11-01 14:31:44 -0300

    Ejercicio hecho y verificado

---
+++ js/Ejercicios/06-ejercicio.js
@@ -0,0 +1,9 @@
+//6 ejercicio
+const numeroPruebaPrimero = parseInt(prompt ("Ingrese un numero:"));
+const numeroPruebaSegundo = parseInt (prompt("Ingrese otro numero:"));
+
+if (numeroPruebaPrimero > numeroPruebaSegundo){
+    document.write ('<p>El ' + numeroPruebaPrimero+ ' es el número más grande</p>');
+
+}else {
+document.write ('<p>El ' + numeroPruebaSegundo+ ' es el número más grande</p>')};
