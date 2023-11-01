ff843ae5b935039aa1e57cc39d67a7e40f28ac01
Maia Federica_G <maiafedericag@gmail.com>
2023-11-01 01:30:57 -0300

    Incopore el princio del codigo

--- js/Ejercicios/09-ejercicio.js
+++ js/Ejercicios/09-ejercicio.js
@@ -1,2 +1,7 @@
 //Escribe un programa que pida una frase y escriba las vocales que aparecen
 //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+
+const frase = prompt ('Ingrese una frase');
+
+console.log (frase.length);
