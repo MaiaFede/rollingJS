4f8df5471fdf5a9d49b27ae56971bf9d2a938d80
Maia Federica_G <maiafedericag@gmail.com>
2023-11-01 13:03:34 -0300

    Ejercicio realizado y verificado

--- js/Ejercicios/10-ejercicio.js
+++ js/Ejercicios/10-ejercicio.js
@@ -1,5 +1,18 @@
-//10.- Escribe un programa que pida un número y nos diga si es divisible 
+//10.- Escribe un programa que pida un número y nos diga si es divisible
 //por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
 
-const numeroPrueba = parseInt(prompt('Ingrese un numero:'));
+const numeroPrueba = parseInt(prompt("Ingrese un numero:"));
 
+let divisores = [2, 3, 5, 7];
+
+for (let i = 0; i < divisores.length; i++) {
+  if (numeroPrueba % divisores[i] == 0) {
+    document.write(
+      ` <p>El ${numeroPrueba} es divisible por ${divisores[i]}</p>`
+    );
+  }/*else {
+    document.write(
+      ` <p> El ${numeroPrueba} no es divisible por ${divisores[i]} </p>`
+    );
+  }* solo para ver con cual no era divisble, asi que lo hice como extra*/
+}
