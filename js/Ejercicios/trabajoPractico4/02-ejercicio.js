ae5eb1d686e184e59f79e7572f244f3817200e45
Maia Federica_G <maiafedericag@gmail.com>
2023-11-27 15:49:30 -0300

    Realice el ejercicio incompleto

---
+++ js/Ejercicios/trabajoPractico4/02-ejercicio.js
@@ -0,0 +1,23 @@
+class Cuenta {
+
+    constructor(){
+    this.titular ="Alex"
+    this.saldo= 0
+    Document.write(`<p>Tu saldo actual es de{this.saldo</p>`)}
+    
+    ingresar(cantidad){ 
+        this.saldo += cantidad 
+        
+        }
+    extraer(cantidad){
+        this.saldo -= cantidad 
+         }
+        informar(){
+        Document.write("Tu saldo actual es de" + //{this.saldo})
+        }
+   }
+
+let alex = new Cuenta()
+alex.ingresar(200)
+alex.extraer(100)
+alex.informar
